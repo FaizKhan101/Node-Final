ef14aaf184c7442baceab9b8647e5922ae90f6d8
Faiz <faizkhan8225@gmail.com>
2023-12-28 14:15:49 +0530

    Add form and retrieve form data and add specific url handler

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
 
-app.use('/add-product', (req, res, next) => {
-    console.log("In the middleware!");
-    res.send("Add Product Page")
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>')
 })
 
-app.use("/", (req, res, next) => {
+app.post("/product", (req, res, next) => {
+    console.log(req.body.title);
+    res.send("test")
+})
+
+app.get("/", (req, res, next) => {
     console.log("Hello from Express");
     res.send("Hello from express!")
 })
