605c9212fd240518c86810077da29e6f8e1c17f9
Faiz <faizkhan8225@gmail.com>
2024-01-01 12:29:37 +0530

    Creating and serving a html document

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,16 @@
+const path = require("path")
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
   console.log(req.body.title);
-  res.send("test");
+  res.redirect("/");
 });
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const path = require("path")
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("Hello from Express");
-  res.send("Hello from express!");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"))
 });
 
 module.exports = router;
