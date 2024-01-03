4e7535ce281903321c76e63e6326c67365c04dd2
Faiz <faizkhan8225@gmail.com>
2024-01-03 22:39:56 +0530

    Add cart page with products

--- controllers/shop.js
+++ controllers/shop.js
@@ -22,41 +22,55 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId
-  Product.findById(prodId, product => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
     res.render("shop/product-detail", {
       pageTitle: product.title,
       path: "/products",
-      product: product
-    })
-  })
-}
+      product: product,
+    });
+  });
+};
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Cart",
-    path: "/cart"
-  })
-}
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (let product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "Cart",
+        path: "/cart",
+        products: cartProducts,
+      });
+    });
+  });
+};
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId
-  Product.findById(prodId, product => {
-    Cart.addToCart(prodId, product.price)
-  })
-  res.redirect("/")
-}
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addToCart(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     pageTitle: "Orders",
-    path: "/orders"
-  })
-}
+    path: "/orders",
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     pageTitle: "CheckOut",
-    path: "/checkout"
-  })
-}
+    path: "/checkout",
+  });
+};
