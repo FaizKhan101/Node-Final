09c811906f121014e33939321f8bf11391e9ad58
Faiz <faizkhan8225@gmail.com>
2024-01-03 11:58:07 +0530

    Add product details page and routes

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,6 +20,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId
+  Product.findById(prodId, product => {
+    res.render("shop/product-detail", {
+      pageTitle: product.title,
+      path: "/products",
+      product: product
+    })
+  })
+}
+
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     pageTitle: "Cart",
