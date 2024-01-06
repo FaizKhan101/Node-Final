77085191a7b79c5f99a28f3c74c5bfff91b8f41b
Faiz <faizkhan8225@gmail.com>
2024-01-06 11:20:56 +0530

    Fetching a single product by findByPk method

--- controllers/shop.js
+++ controllers/shop.js
@@ -31,12 +31,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
       res.render("shop/product-detail", {
         pageTitle: product.title,
         path: "/products",
-        product: product[0],
+        product: product,
       });
     })
     .catch((err) => {
