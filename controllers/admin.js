a42e3bc0226f3e5ca03edcb0b1b7522851c99237
Faiz <faizkhan8225@gmail.com>
2024-01-10 10:59:29 +0530

    able to delete a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -89,7 +89,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then((result) => {
       console.log("Product deleted!");
       res.redirect("/admin/products");
